afe0d9905c9d9f122cf55a7f061b1a893078aeb5
emraheroglu <emrherglu_78@hotmail.com>
2021-11-19 16:26:51 +0300

    UDP Socket Example

---
+++ UDP_SocketExample/UDP_SocketExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UDP_SocketExample_Core;
+
+namespace UDP_SocketExample
+{
+    class Program
+    {
+        private const int BufferSize = 1024;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 'S' for server 'C' for client");
+
+            string input = Console.ReadLine();
+
+            if (input.Equals("S"))
+            {
+                Console.WriteLine("Starting server");
+
+                UDPSocket server = new UDPSocket("127.0.0.1", 50000);
+                string receivedMessage = server.Echo();
+
+                Console.WriteLine($"Server received {receivedMessage}");
+            }
+            else if (input.Equals("C"))
+            {
+                Console.WriteLine("Starting client");
+
+                UDPSocket client = new UDPSocket("127.0.0.1", 60000);
+                client.Send("Hello World", "127.0.0.1", 50000);
+                string receivedMessage = client.Listen();
+
+                Console.WriteLine($"Client received {receivedMessage}");
+            }
+            else
+            {
+                Console.WriteLine("Unexpected input!");
+            }
+            Console.WriteLine("Press any key to quit!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UDP_SocketExample/UDP_SocketExample_Core/UDPSocket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_SocketExample_Core
+{
+
+    public class UDPSocket
+    {
+        public const int SIO_UDP_CONNRESET = -1744830452;
+        Socket _socket;
+        const int BufferSize = 1024;
+
+        public UDPSocket(string ipAddress, int portNum)
+        {
+            _socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
+            _socket.IOControl(
+                (IOControlCode)SIO_UDP_CONNRESET,
+                new byte[] { 0, 0, 0, 0 },
+                null);
+            IPAddress parsedIpAddress = IPAddress.Parse(ipAddress);
+            IPEndPoint serverEndPoint = new IPEndPoint(parsedIpAddress, portNum);
+            _socket.Bind(serverEndPoint);
+        }
+
+        public string Listen()
+        {
+            byte[] receivedBytes = new byte[BufferSize];
+            _socket.Receive(receivedBytes);
+
+            string receivedMessage = Encoding.ASCII.GetString(receivedBytes);
+            return receivedMessage;
+        }
+
+        public void Send(string messageToSend, string ipAddress, int portNum)
+        {
+            IPAddress serverIpAdress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint serverEndPoint = new IPEndPoint(serverIpAdress, portNum);
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(messageToSend);
+            _socket.SendTo(bytesToSend, serverEndPoint);
+        }
+
+        public string Echo()
+        {
+            byte[] receivedBytes = new byte[BufferSize];
+            EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            _socket.ReceiveFrom(receivedBytes, ref clientEndPoint);
+
+            // log received message to user
+            string receivedMessage = Encoding.ASCII.GetString(receivedBytes);
+
+            return receivedMessage;
+        }
+    }
+}
